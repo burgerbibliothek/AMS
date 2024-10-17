0ea21857067f2044fed3bbd4916433a1ddbf9c0d
Giò <mail@gionathandiani.name>
2024-10-17 14:54:32 +0200

    imports UI schöner

--- app/Filament/Resources/Import/ImportsResource/Pages/ListImportsDetails.php
+++ app/Filament/Resources/Import/ImportsResource/Pages/ListImportsDetails.php
@@ -9,6 +9,12 @@ use Filament\Resources\Pages\ListRecords;
 use Filament\Infolists;
 use Filament\Infolists\Infolist;
 
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\Split;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Support\Enums\FontWeight;
+use Filament\Infolists\Components\Grid;
+
 class ListImportsDetails extends ViewRecord
 {
     protected static string $resource = ImportsResource::class;
@@ -17,11 +23,35 @@ class ListImportsDetails extends ViewRecord
     {
         return $infolist
             ->schema([
+                Section::make([
+                    Grid::make([
+                        'default' => 1,
+                        'sm' => 2,
+                        'md' => 4,
+                        'lg' => 4,
+                        'xl' => 4,
+                        '2xl' => 4,
+                    ])->schema([
+                        TextEntry::make('total_rows')
+                            ->weight(FontWeight::Bold),
+                        TextEntry::make('successful_rows')
+                            ->weight(FontWeight::Bold),
+                        TextEntry::make('created_at')
+                            ->dateTime('d.m.Y H:i:s')
+                            ->weight(FontWeight::Bold),
+                            TextEntry::make('completed_at')
+                            ->dateTime('d.m.Y H:i:s')
+                            ->weight(FontWeight::Bold),
+                    ])
+                ]),
+
+                /*
                 Infolists\Components\TextEntry::make('file_name'),
                 Infolists\Components\TextEntry::make('file_path'),
                 Infolists\Components\TextEntry::make('total_rows'),
                 Infolists\Components\TextEntry::make('successful_rows')
                     ->columnSpanFull(),
+                    */
             ]);
     }
     
