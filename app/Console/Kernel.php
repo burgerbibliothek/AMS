a8b3b47dd2efb3789a4675c2e05b700fc1083a10
Giò <code+github@diani.xyz>
2026-06-09 17:13:43 +0200

    update schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('queue:work --stop-when-empty')
+        $schedule->command('queue:work --tries=5 --memory=1024 --timeout=90 --stop-when-empty --backoff=300')
             ->everyMinute()
             ->withoutOverlapping();
     }
