ea83c44f246c36bc30dc395612c43451b55cd393
Giò <7964115+giodi@users.noreply.github.com>
2026-09-16 14:28:20 +0200

    escape values for HTML rendering

--- app/Casts/ArkRevision.php
+++ app/Casts/ArkRevision.php
@@ -15,14 +15,14 @@ class ArkRevision implements CastsAttributes
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
         $json = json_decode($value, true);
-        $value = '<strong>URI:</strong> '.$json['uri'].'<br>';
+        $value = '<strong>URI:</strong> '.htmlspecialchars($json['uri']).'<br>';
 
         if (! empty($json['metadata'])) {
-            $value .= '<strong>Metadata:</strong> <code>'.$json['metadata'].'</code><br>';
+            $value .= '<strong>Metadata:</strong> <code>'.htmlspecialchars($json['metadata']).'</code><br>';
         }
 
         if (! empty($json['http-status'])) {
-            $value .= '<strong>HTTP-Status:</strong> <code>'.$json['http-status'].'</code>';
+            $value .= '<strong>HTTP-Status:</strong> <code>'.htmlspecialchars($json['http-status']).'</code>';
         }
 
         return $value;
